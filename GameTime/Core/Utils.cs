35d8d432676ac8493c9f4cda616af8dc969d6bd4
Daniel Martinez Cequiel <danimc100@gmail.com>
2021-08-24 19:58:31 +0200

    format

--- GameTime/Core/Utils.cs
+++ GameTime/Core/Utils.cs
@@ -10,12 +10,37 @@ namespace GameTime.Core
     {
         public static string TimeFormat(TimeSpan t)
         {
+            int hours = t.Days * 24 + t.Hours;
+            string format;
+
+            if(hours < 99)
+            {
+                format = "{0:D2}:{1:D2}:{2:D2}";
+            }
+            else
+            {
+                if(hours < 999)
+                {
+                    format = "{0:D3}:{1:D2}:{2:D2}";
+                }
+                else
+                {
+                    format = "{0:D4}:{1:D2}:{2:D2}";
+                }
+            }
+
             return string.Format(
-                "Días {0} - {1:D2}:{2:D2}:{3:D2}",
-                t.Days,
-                t.Hours,
+                format,
+                (t.Days * 24) + t.Hours,
                 t.Minutes,
                 t.Seconds);
+
+            //return string.Format(
+            //    "Días {0} - {1:D2}:{2:D2}:{3:D2}",
+            //    t.Days,
+            //    t.Hours,
+            //    t.Minutes,
+            //    t.Seconds);
         }
     }
 }
