843f5f6ed58674aaf054221a3b5d53763026ca14
Daniel Martinez Cequiel <danimc100@gmail.com>
2023-09-06 12:44:07 +0200

    Bug pérdida del foco en cada evento de timer.

--- GameTime/FrMain.cs
+++ GameTime/FrMain.cs
@@ -144,7 +144,6 @@ namespace GameTime
             {
                 game.NotifyForm = new FrNotify();
             }
-            game.NotifyForm.TopMost = true;
             game.NotifyForm.Title = $"{game.Title} / {game.Name}";
             game.NotifyForm.Time = game.PartialTime.ToString();
             game.NotifyForm.SetLocationAndShow(this, game.ActiveNum);
